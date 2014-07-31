996f45f9afef5c8817a69f0a007977e111c5e08a
mayever <mayever7@bapul.net>
2014-07-31 17:35:31 +0900

    fix deploy.sj

--- deploy.sh
+++ deploy.sh
@@ -128,6 +128,7 @@ fi
 # 5. KuduSync to Target
 "$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
 exitWithMessageOnError "Kudu Sync to Target failed"
+
 ##################################################################################################################################
 
 # Post deployment stub
